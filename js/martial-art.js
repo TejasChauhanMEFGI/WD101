beffa30ec2a6208003b6c69a5b62a812cac92e0a
TejasChauhanMEFGI <90142983+TejasChauhanMEFGI@users.noreply.github.com>
2021-09-05 19:55:05 +0530

    Update martial-art.js

--- js/martial-art.js
+++ js/martial-art.js
@@ -8,7 +8,7 @@ let loadImage = (src, callback) => {
 };
 
 let imagePath = (frameNumber, animation) => {
-  return "/WD101/images/martial-arts/" + animation + "/" + frameNumber + ".png";
+  return "../images/martial-arts/" + animation + "/" + frameNumber + ".png";
 };
 
 let frames = {
